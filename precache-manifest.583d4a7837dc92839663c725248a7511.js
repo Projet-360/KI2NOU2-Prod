720acabc6bf3c2d691d6f2ee8ceda0e4fd164eb6
Projet 360 <37069190+Projet-360@users.noreply.github.com>
2022-06-14 07:04:08 +0200

    First

---
+++ precache-manifest.583d4a7837dc92839663c725248a7511.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64894bd56550262a736b",
+    "url": "bundle.css"
+  },
+  {
+    "revision": "64894bd56550262a736b",
+    "url": "bundle.js"
+  },
+  {
+    "revision": "519118d9e68f5a36c557066429b22d8e",
+    "url": "favicon.png"
+  },
+  {
+    "url": "icon_120x120.49bfa5d5e18e1d5135bfe35dcdb14594.png"
+  },
+  {
+    "url": "icon_128x128.1e2d5a8220c8d46ee4e1565e5f028bca.png"
+  },
+  {
+    "url": "icon_152x152.3aa3a5f00f57cbd62cdc4948cc5bf315.png"
+  },
+  {
+    "url": "icon_167x167.359109dc0c88f7c2b8a7dd56747300f8.png"
+  },
+  {
+    "url": "icon_180x180.fe2f6fb2f37f8d7089a790d5a8492fd7.png"
+  },
+  {
+    "url": "icon_192x192.a6725fc526fc720d6f9fb7e74de7aa53.png"
+  },
+  {
+    "url": "icon_192x192.d3e9a0315f53f6870c4ed057ab30ab77.png"
+  },
+  {
+    "url": "icon_32x32.d7003cdd2fdf700dab5dd7eb97894a51.png"
+  },
+  {
+    "url": "icon_512x512.d8b43967f1ba7d1b89fd5beff0ea7e44.png"
+  },
+  {
+    "url": "icon_64x64.5cf1b663d923a686173a2e4c7f686d89.png"
+  },
+  {
+    "url": "icon_96x96.a36b376eca47ca8f991ba5188a9a46f1.png"
+  },
+  {
+    "revision": "70deac795d01691e0074f1b4cd33d75e",
+    "url": "index.html"
+  },
+  {
+    "url": "manifest.4257100c848a28a4cb4ae2c95dcafee1.json"
+  }
+]);
